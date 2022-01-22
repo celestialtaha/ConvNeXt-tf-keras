62fd24cc1d220f5d6d17007beada605f3ef42033
Taha Samavati <tahasamavati11@yahoo.com>
2022-01-22 03:52:35 +0330

    Add drop_path and layer_scale to the create_model

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow.keras.layers as layers
 from blocks import *
 
-def create_convnext_model(input_shape=(224, 224, 3), depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], num_classes=1000):
+def create_convnext_model(input_shape=(224, 224, 3), depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], num_classes=1000, drop_path=0., layer_scale_init_value=1e-6):
 
     input = layers.Input(shape=input_shape)
 
@@ -10,22 +10,22 @@ def create_convnext_model(input_shape=(224, 224, 3), depths=[3, 3, 9, 3], dims=[
     y = layers.Conv2D(dims[0], kernel_size=4, strides=4)(input)
     y = layers.LayerNormalization(epsilon=1e-6)(y)
     for i in range(depths[0]):
-        y = ConvNeXt_Block(dims[0])(y)
+        y = ConvNeXt_Block(dims[0], drop_path, layer_scale_init_value)(y)
 
     # downsample + res3
     y = Downsample_Block(dims[1])(y)
     for i in range(depths[1]):
-        y = ConvNeXt_Block(dims[1])(y)
+        y = ConvNeXt_Block(dims[1], drop_path, layer_scale_init_value)(y)
 
     # downsample + res4
     y = Downsample_Block(dims[2])(y)
     for i in range(depths[2]):
-        y = ConvNeXt_Block(dims[2])(y)
+        y = ConvNeXt_Block(dims[2], drop_path, layer_scale_init_value)(y)
     
     # downsample + res5
     y = Downsample_Block(dims[3])(y)
     for i in range(depths[3]):
-        y = ConvNeXt_Block(dims[3])(y)
+        y = ConvNeXt_Block(dims[3], drop_path, layer_scale_init_value)(y)
 
     y = layers.GlobalAveragePooling2D()(y)
     # final norm layer
